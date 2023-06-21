7d87b87f7b4b2860dfba66052fabed13b96cfca0
Basavaraj S K <basavask@in.ibm.com>
2023-06-21 14:19:28 +0530

    fix(vite): added vite configurations and removed unwanted things

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
-import { Carousel } from "./components/Carousel";
+import { Carousel } from "./components/carousel/Carousel";
+import Header from "./components/header/header";
 
 import { slides } from "./data/carouselData.json";
 
 function App() {
   return (
-    <div className="App">
-      <Carousel data={slides} />
-    </div>
+    <>
+      <div>
+        <Header />
+      </div>
+      <div className="App">
+        <Carousel data={slides} />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/header/header.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import "./header.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className="header">
+        <h1>CMS</h1>
+      </header>
+    </Fragment>
+  );
+};
+
+export default Header;
