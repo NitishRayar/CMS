8ed27bf5d41759fb0bd7acdc1380c16c48d2b7fe
Basavaraj S K <basavask@in.ibm.com>
2023-07-09 10:32:05 +0530

    fix: style changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,19 @@ import ButtonComponent from "./components/button/buttonComponent";
 import Slide from "./components/slide/slide";
 import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
       <Header />
       <Slide />
-      <About />
-      <ButtonComponent label="Our Achievements" />
+      <div>
+        <About />
+      </div>
+      <div>
+        <ButtonComponent label="Our Achievements" />
+      </div>
       <AchievementCardComponent />
       <ButtonComponent label="Feature Events" />
       <AchievementCardComponent />
--- src/components/slide/slide.jsx
+++ src/components/slide/slide.jsx
@@ -3,35 +3,45 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { images } from "../../data/slideImage.json";
 import classes from "./slide.module.css";
+import styles from "./custom.slide.module.css";
 
 const Slide = () => {
   const [currentIndex, setCurrentIndex] = useState();
-  
 
   function handleChange(index) {
     setCurrentIndex(index);
   }
-  const renderImage= (image)=>{
-    return new URL(image,import.meta.url);
-  }
-  const renderSlides = images.map((image) => (
-    <div key={image.alt} className={classes.image}>
-      <img src={renderImage(image.url)} alt={image.alt} />
-      <p className="legend">{image.label}</p>
-    </div>
-  ));
+  const renderImage = (image) => {
+    return new URL(image, import.meta.url);
+  };
+  const renderSlides = images.map((image) => {
+    return (
+      <div key={image.alt} className={classes.image}>
+        <img
+          src={image?.url}
+          alt={image.alt}
+          style={{ width: "100%", height: "60vh" }}
+        />
+        <p className="legend">{image.label}</p>
+      </div>
+    );
+  });
+
   return (
     <>
-      <Carousel
-        showArrows={true}
-        autoPlay={true}
-        infiniteLoop={true}
-        selectedItem={images[currentIndex]}
-        onChange={handleChange}
-        showThumbs={false}
-      >
-        {renderSlides}
-      </Carousel>
+      <div>
+        <Carousel
+          className={styles.customCarousel}
+          showArrows={true}
+          autoPlay={true}
+          infiniteLoop={true}
+          selectedItem={images[currentIndex]}
+          onChange={handleChange}
+          showThumbs={false}
+        >
+          {renderSlides}
+        </Carousel>
+      </div>
     </>
   );
 };
