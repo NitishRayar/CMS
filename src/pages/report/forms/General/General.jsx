2419d8c15a9ff126738e0272d88fedb12cbee04e
bhojaraj <bhojaraj.gouda@gmail.com>
2023-07-30 21:57:50 +0530

    add place and ward inputs in report form

--- src/pages/report/forms/General/General.jsx
+++ src/pages/report/forms/General/General.jsx
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { Places } from "../../../../data/Place.json";
+// import { Ward } from "../../../../data/Ward.json";
 import "./style.css";
 const General = ({ onClose }) => {
+  const [wards, setWards] = useState();
+  const renderPlaces = Places.map(place => {
+    return (
+      <option key={place.placeId} value={place.placeName}>
+        {place.placeNameK}
+      </option>
+    );
+  });
+
+  const handleSelect = e => {
+    const { value } = e.target;
+    const wardsData = Places.filter(place => {
+      return place.placeName === value;
+    });
+    setWards(wardsData[0].wards);
+  };
+
+  const renderWards =
+    wards &&
+    wards.map(ward => {
+      return (
+        <option key={ward.wardId} value={ward.wardName}>
+          {ward.wardNameK}
+        </option>
+      );
+    });
+
   return (
     <div className="general-popup-container">
       <div className="general-popup-content">
         <div className="general-form">
           <div className="general-form-inputContainer">
-            <input type="text" placeholder="Place" />
-            <select id="Place">
-              <option value="">Select Place</option>
-              <option value="content">content</option>
-              <option value="content">content</option>
-              <option value="content">content</option>
-              <option value="content">content</option>
+            <span>Place:</span>
+            <select id="Place" onChange={handleSelect}>
+              <option value="default">Select Place</option>
+              {renderPlaces}
             </select>
           </div>
           <div className="general-form-inputContainer">
-            <input type="text" placeholder="Place" />
+            <span>Ward:</span>
             <select id="ward">
               <option value="">Select Ward</option>
-              <option value="content">content</option>
-              <option value="content">content</option>
-              <option value="content">content</option>
-              <option value="content">content</option>
+              {renderWards}
             </select>
           </div>
         </div>
